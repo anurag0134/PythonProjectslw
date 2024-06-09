ae654db5cb85b6863bdae79cf2b6f36b1c6475a3
Anurag Deshmukh <111648310+anurag0134@users.noreply.github.com>
2024-06-09 16:39:31 +0530

    A form using python

---
+++ python1task.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:")
+contact=input("Enter your contact no:")
+email=input("Enter your mail Id:")
+whatsapp=input("Enter your whatsapp number:")
+
+print(name)
+print(contact)
+print(email)
+print(whatsapp)
